732093327032441f8a0e0a41026d716f1972faf0
Anthony2be <50805238+Anthony2be@users.noreply.github.com>
2022-11-09 21:31:59 -0800

    Update server.ts

--- server.ts
+++ server.ts
@@ -7,10 +7,18 @@ export interface Payload{
 
 const rooms = new Map<string, WebSocket[]>();
 
+function removeFromRooms(ws: WebSocket){
+  rooms.forEach(wsArray => {
+    if(wsArray.includes(ws)){
+      wsArray.splice(wsArray.indexOf(ws), 1);
+    }
+  });
+}
+
 function broadcastAll(message: string) {
   for (const room of rooms.values()) {
     for (const user of room) {
-      user.send(message);
+      user.send(JSON.stringify({type:"MESSAGE", data: message}))
     }
   }
 }
@@ -18,43 +26,41 @@ function broadcastAll(message: string) {
 function broadcastRoom(ws: WebSocket,message: string) {
   rooms.forEach(wsArray => {
     if(wsArray.includes(ws)){
-      wsArray.forEach(user => user.send(message));
+      wsArray.forEach(user => user.send(JSON.stringify({type:"MESSAGE", data: message})));
     }
   });
 }
 
 function logError(msg: string) {
   console.log(msg);
-  Deno.exit(1);
 }
 function handleConnected(ws: WebSocket) {
   console.log("Connected to client ...");
   rooms.get("general")?.push(ws);
-  ws.send("Server: Welcome to the chat");
+  ws.send(JSON.stringify({type:"MESSAGE", data: "Server: Welcome to the chat"}));
 }
 function handleMessage(ws: WebSocket, data: string) {
   const { type, payload }: {type:string, payload:Payload } = JSON.parse(data);
   switch (type) {
     case "join-room":
       console.log("h")
-      rooms.forEach(wsArray => {
-        if(wsArray.includes(ws)){
-          wsArray.splice(wsArray.indexOf(ws), 1);
-        }
-      });
+      removeFromRooms(ws);q
       if (rooms.has(payload.data)) {
-        rooms.set(payload.data, []);
+        rooms.get(payload.data)?.push(ws);
       }
       else{
-        rooms.get(payload.data)?.push(ws);
+        rooms.set(payload.data, [ws]);
       }
       broadcastRoom(ws, `${payload.username} joined the room`);
       console.log(rooms);
       break;
-    case "message": {
+
+    case "message": 
       broadcastRoom(ws, `${payload.username}: ${payload.data}`);
       break;
-    }
+
+    case "get-rooms":
+      ws.send(JSON.stringify(JSON.stringify({type:"ROOMS", data: [...rooms.keys()]})))
   }
 }
 function handleError(e: Event | ErrorEvent) {
@@ -67,8 +73,14 @@ function reqHandler(req: Request) {
   const { socket: ws, response } = Deno.upgradeWebSocket(req);
   ws.onopen = () => handleConnected(ws);
   ws.onmessage = (m) => handleMessage(ws, m.data);
-  ws.onclose = () => logError("Disconnected from client ...");
-  ws.onerror = (e) => handleError(e);
+  ws.onclose = () => {
+    removeFromRooms(ws);
+    logError("Disconnected from client ...");
+  }
+  ws.onerror = (e) => {
+    removeFromRooms(ws);
+    handleError(e);
+  }
   return response;
 }
 console.log("Waiting for client ...");
